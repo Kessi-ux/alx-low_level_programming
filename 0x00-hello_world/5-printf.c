3a095429b93d1fc048f3c632f99e547e28172e46
Kessi-ux <chukwubueze.uchenyi.231361@unn.edu.ng>
2023-09-13 03:36:03 -0700

    betty style

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 /**
  *main- A c program that displays information to the output
- *Description: It formats and prints exactly: with proper grammar, but the outcome is a piece of art,
+ *Description: It formats and prints exactly
+ *: with proper grammar, but the outcome is a piece of art,
  *Return: Returns 0 as success value
  */
 int main(void)
