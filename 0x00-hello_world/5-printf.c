0d1a7d4e06ef80d11fae4adb02539b90cd74691a
Kessi-ux <chukwubueze.uchenyi.231361@unn.edu.ng>
2023-09-13 03:30:06 -0700

    5-printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main- A c program that displays information to the output
+ *Description: It formats and prints exactly: with proper grammar, but the outcome is a piece of art,
+ *Return: Returns 0 as success value
+ */
+int main(void)
+{
+	printf("\nwith proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
