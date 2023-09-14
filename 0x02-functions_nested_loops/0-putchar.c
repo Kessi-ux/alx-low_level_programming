bca4bf061b676301a8f07af9f6bfe4b0074821b4
Kessi-ux <chukwubueze.uchenyi.231361@unn.edu.ng>
2023-09-13 18:17:11 -0700

    0-putchar is now executable

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A c program
+ * Description: It displays _putchar, followed by a new line
+ * Return: Returns 0 as succes value
+ */
+int main(void)
+{
+        printf("_putchar\n");
+        return (0);
+}
